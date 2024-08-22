173bfee7cfe3329a1a401b98ddf9f5b3af10fe41
ElyasShamal <Elyas.shamal2@gmail.com>
2024-08-21 23:50:18 -0400

    completed

---
+++ ShamalHelloWorld.java
@@ -0,0 +1,14 @@
+
+
+/*
+ * Author: Elyas Shamal
+ * Date: 8/19/2024
+ * Purpose: This program prints "Hello World!" to the console.
+ * */
+public class ShamalHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+
+}
---
+++ ShamalPerson.java
@@ -0,0 +1,19 @@
+/* 
+ * Author: Elyas Shamal
+ * Date: 08/19/2024
+ * Purpose: This program displays personal information to the console using System.out.print() and System.out.println()
+ * 
+ * */
+
+public class ShamalPerson {
+
+	public static void main(String[] args) {
+		
+		System.out.print("My name is Elyas Shamal \n"); // \n escape sequence is used to move the output to the next line within a single System.out.print().
+		System.out.println("I live in Durham, NC "); // System.out.println() automatically moves to the next line after printing.
+		System.out.print("My phone number is 111-111-1111 \n");
+		System.out.println("My major is Information Technology Software Development");
+		System.out.print("My dream profession is to be a Software Engineer");		
+	}
+
+}
